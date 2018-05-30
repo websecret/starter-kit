d36333176dc4dbf46375d29d355edd895db9f4a8
Vladilen <blax56@gmail.com>
2018-05-30 10:00:04 +0300

    refs #6391

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.options({
-    cleanCss: {
-        level: {
-            1: {
-                specialComments: 'none'
-            }
-        }
-    },
-    purifyCss: true
-});
-
 mix.js('resources/assets/admin/js/app.js', 'public/assets/admin/js/app.js');
 
 mix.sass('resources/assets/admin/sass/app.scss', 'public/assets/admin/css/app.css');

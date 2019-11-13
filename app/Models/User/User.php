4c979baf2d7c2191560f4737ec7d8ace9c664e4f
Viktar Tairau <v.tairau@websecret.by>
2019-11-13 12:48:04 +0300

    fix role attribute

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable
 
     public function getRoleAttribute()
     {
-        return $this->roles()->first();
+        return $this->roles->first();
     }
 
     public function canAccessRoute($routeName)
